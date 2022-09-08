b9b5007b879757eb5975e75628185145854b825d
igorgussi <igorgussi2017@gmail.com>
2022-09-08 16:22:14 -0300

    Atualização de arquivos

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 DateTime dataatual = DateTime.Now; //Puxa a data atual do computador. Caso queria adicionar dias na data atual .AddDays(5)
 Console.WriteLine($"A data e hora atual são: {dataatual}"); // Caso queira formatar apenas para aparecer dd/MM/yyyy:  .ToString("dd/MM/yyyy") ou .ToString("HH/mm") para mostrar somente a hora.
 
-/*
+
 //Declarando variáveis
 string apresentacao = "Olá, seja bem vindo";
 int quantidade = 1;
@@ -18,7 +18,7 @@ Console.WriteLine($"O valor da variável quantidade é: {quantidade}");
 Console.WriteLine($"O valor da variável altura é:{altura.ToString("0.00")}"); //Tratamento para colocar casa decimal em variável do tipo double: ToString("0.00")
 Console.WriteLine($"O valor da variável preço é: {preco}");
 Console.WriteLine($"O valor da variável condição é: {condicao}");
-*/
+
 
 //Instanciando uma classe 
 /*Pessoa p = new Pessoa();
