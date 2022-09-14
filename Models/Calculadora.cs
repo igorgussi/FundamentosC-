3109eda628bd87942dc86d526e3fe518eb626042
igorgussi <igorgussi2017@gmail.com>
2022-09-14 14:17:52 -0300

    Atualização do código

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -32,10 +32,40 @@ namespace Fundamentos.Models
             Console.WriteLine($"{x} * {y} = {resultado.ToString("0.00")}");
         }
 
+        //Fazendo Potencia 
         public void Potencia (double x, double y)
         {
           double resultado = Math.Pow(x, y); //Utilizado para calcular potência
           Console.WriteLine($"Potencia: \n {x}^{y} = {resultado}");
         }
+
+        //Inserindo funções trigonométricas 
+        public void Seno(double angulo)
+        {
+          double radiano = angulo * Math.PI / 180;
+          double seno = Math.Sin(radiano);
+          Console.WriteLine($"Seno de {angulo} = { Math.Round (seno, 4)}");
+        }
+
+        public void Coseno(double angulo)
+        {
+          double radiano = angulo * Math.PI / 180;
+          double coseno = Math.Cos(radiano);
+          Console.WriteLine($"Seno de {angulo} = {Math.Round (coseno, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+          double radiano = angulo * Math.PI / 180;
+          double tangente = Math.Tan(radiano);
+          Console.WriteLine($"Seno de {angulo} = { Math.Round (tangente, 4)}");
+        }
+
+        //Raiz Quadrada 
+        public void RaizQuadrada(double x){
+          resultado = Math.Sqrt(x); //Função para encontrar raiz quadrada
+          Console.WriteLine($"Raiz Quadrada de {x} = {resultado}");
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,53 +1,75 @@
 ﻿using Fundamentos.Models;  //Mostrando o namespace ao qual as classes estão
 
 //Criando uma calculadora 
-
 Calculadora calc = new Calculadora();
 double x = 0, y = 0;
 string operacao;
 
 Console.WriteLine ("Digite o primeiro numero: ");
 x = Convert.ToDouble (Console.ReadLine());
-Console.WriteLine("\nSelecione a operação desejada: \n + --> Soma\n - --> Subtração\n / --> Divisão\n * --> Multiplicação\n ^ --> Potência");
+Console.WriteLine("\nSelecione a operação desejada: \n 1(+) --> Soma\n 2(-) --> Subtração\n 3(/) --> Divisão\n 4(*) --> Multiplicação\n 5(^) --> Potência\n 6 --> Funções Trigonométricas\n 7 --> Raiz Quadrada \n\n\n 0 --> Sair");
 operacao = Console.ReadLine();
-Console.WriteLine ("\nDigite o segundo numero: ");
-y = Convert.ToDouble (Console.ReadLine());
 
 switch (operacao){
-    case "+":
+    case "1" or "+":
+    Console.WriteLine ("\nDigite o segundo numero: ");
+    y = Convert.ToDouble (Console.ReadLine());
     calc.Somar(x, y);
     break;
 
-    case "-":
+    case "2" or "-":
+    Console.WriteLine ("\nDigite o segundo numero: ");
+    y = Convert.ToDouble (Console.ReadLine());
     calc.Subtrair(x, y);
     break;
 
-    case "*":
+    case "3" or "*":
+    Console.WriteLine ("\nDigite o segundo numero: ");
+    y = Convert.ToDouble (Console.ReadLine());
     calc.Multiplicar(x, y);
     break;
 
-    case "/":
+    case "4" or "/":
+    Console.WriteLine ("\nDigite o segundo numero: ");
+    y = Convert.ToDouble (Console.ReadLine());
     calc.Dividir(x, y);
     break;
 
-    case "^":
+    case "5" or "^":
+    Console.WriteLine ("\nDigite o expoente: ");
+    y = Convert.ToDouble (Console.ReadLine());
     calc.Potencia(x, y);
+    break;
+
+    case "6":
+    int opcTrigonometrica;
+
+    Console.WriteLine("Selecione a operção Trigonométrica: \n 1 - Seno\n 2 - Coseno\n 3 - Tangente");
+    opcTrigonometrica = Convert.ToInt32(Console.ReadLine());
+        if (opcTrigonometrica == 1){
+            calc.Seno(x);
+        }
+        else if(opcTrigonometrica == 2){
+            calc.Coseno(x);
+        }
+        else if (opcTrigonometrica == 3){
+            calc.Tangente(x);
+        }
+    break;
+
+    case "7":
+    calc.RaizQuadrada(x);
+    break;
+
+    case "0":
+    Console.WriteLine("Obrigado, até mais!");
+    Console.ReadKey();
     break;
 
     default:
     Console.WriteLine($"Operação '{operacao}' não pode ser encontrada!");
     break;
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
